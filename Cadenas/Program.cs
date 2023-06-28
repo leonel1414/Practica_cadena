3f3bf6c8ea0b925943cedcf47c531a2be06f8790
LeonelGirett <82616033+leonel1414@users.noreply.github.com>
2023-06-28 19:43:28 -0300

    Agregar archivos de proyecto.

---
+++ Cadenas/Program.cs
@@ -0,0 +1,10 @@
+﻿string? str = null;
+string str3 = System.String.Empty;
+string str4 = "";
+string str5 = " ";
+
+
+//tipo y salida
+
+Console.WriteLine($"str5: {str5}");
+Console.WriteLine(str5.GetType());
