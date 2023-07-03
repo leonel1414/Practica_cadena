c28bccd4afac396414ff1b5dfd2ca3acb33777cc
LeonelGirett <82616033+leonel1414@users.noreply.github.com>
2023-07-03 20:31:45 -0300

    Modificaciones en String y Arrays

--- Cadenas/Program.cs
+++ Cadenas/Program.cs
@@ -25,8 +25,55 @@ Console.WriteLine(content);
 //Salto de fila
 string rows = ("contenido1 contenido1\r\ncontenido2 contenido2\r\nRow 3");
 Console.WriteLine(rows);
-*/
+
 
 //interpolación
 var persona = (nombre: "Leonel", apellido: "Girett", edad: "30", lenguaje: "C#");
 Console.WriteLine($"Hola mi nombre es, {persona.nombre}, tengo {persona.edad} años");
+
+
+//Arrays
+string[] names = new string[2];
+names[0] = "Leonel Girett";
+names[1] = "Belen Gonzalez";
+
+foreach (string name in names) { 
+    Console.WriteLine(name);
+}
+Console.WriteLine("\n");
+Console.WriteLine("FOR CLASICO");
+for (int i = 0; i < names.Length; i++) {
+    Console.WriteLine(names[i]);
+}
+
+int[] numbers = { 4, 6, 5, 2, 3, 1 };
+//Array original
+foreach (var number in numbers) {
+    Console.WriteLine(number);
+}
+Console.WriteLine("\n");
+Console.WriteLine("*** Array Ordenada***");
+
+Array.Sort(numbers);
+foreach (int i in numbers) {
+    Console.WriteLine(i);
+}
+*/
+
+// Mas tipo de Arrays
+int[,] miArray = new int[2,2]; //1 2
+                               //3 4
+
+miArray[0, 0] = 1;
+miArray[0, 1] = 2;
+miArray[1, 0] = 3;
+miArray[1, 1] = 4;
+
+for (int i = 0; i < 2; i++) {
+     for (int j = 0; j < 2; j++)
+    {
+        Console.Write(miArray[i, j] + " ");
+    }
+     Console.WriteLine("");
+}
+
